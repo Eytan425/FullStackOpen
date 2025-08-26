394eff6fff208f66714a5cbc05de88d8c5997d81
Eytan Benittah <eytanbenittah@gmail.com>
2025-08-26 21:35:59 +0300

    Finished c of part 1

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,18 +1,29 @@
-const Hello = (props) =>{
+import { useState } from "react"
+
+
+const Display = (props) => {
   return (
-    <div>
-      <p>Hello {props.name}</p>
-    </div>
+    <div>{props.counter}</div>
   )
 }
+
+
 
 const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter-1)
+  const setToZero = () => setCounter(0)
+
   return (
     <div>
-      <Hello name = 'Eytan'/>
+      <Display counter={counter}/>
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
+      <button onClick={decreaseByOne}>minus</button>
+      
     </div>
   )
 }
-
 export default App
-
